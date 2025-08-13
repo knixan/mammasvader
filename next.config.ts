1bb6a7e2a391e10999220c231bcf92ec5a008f42
knixan <josefineeriksson@live.se>
2025-08-13 14:37:17 +0200

    bytt till Image från img

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+// next.config.mjs
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "openweathermap.org",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
